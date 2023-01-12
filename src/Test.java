b51da1a29192566c60da0c03d3de991711670ca1
Gabriel C <gabriel.camargo96@hotmail.com.br>
2023-01-12 15:35:41 -0300

    feat: create class Test

---
+++ src/Test.java
@@ -0,0 +1,39 @@
+public class Test {
+    private int questionNumber;
+    private Question[] questions;
+    public String teacher;
+    private String subject;
+
+    public Test(int questionNumber, String subject) {
+        this.questionNumber = questionNumber;
+        this.questions = new Question[questionNumber];
+        this.subject = subject;
+    }
+
+    public int getQuestionNumber() {
+        return this.questionNumber;
+    }
+
+    public Question[] getQuestions() {
+        return this.questions;
+    }
+
+    public void addQuestion(Question question) {
+        if (!question.subject.equals(this.subject)) {
+            System.out.println("Não é possível colocar questões de outra disciplina na prova.");
+        } else {
+            for (Question current : this.questions) {
+                if (current.questionStatement.equals("")) {
+                    current.questionStatement = question.questionStatement;
+                    current.subject = question.subject;
+                    return;
+                }
+            }
+            System.out.println("Essa prova já está totalmente preenchida de questões.");
+        }
+    }
+
+    public void removeQuestion(int index) {
+        this.questions[index] = new Question();
+    }
+}
