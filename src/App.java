c2c749192876efbaa2111221ed0e059daf171670
Gabriel C <gabriel.camargo96@hotmail.com.br>
2023-01-12 15:48:25 -0300

    feat: main script to test classes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,33 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Question question1 = new Question();
+        question1.questionStatement = "Qual o significado da vida, do universo e tudo mais?";
+        question1.subject = "Cultura pop";
+        question1.setResolution("42");
+
+        Question question2 = new Question();
+        question2.questionStatement = "Quanto custa um jantar no restaurante no fim do universo?";
+        question2.subject = "Cultura pop";
+
+        Question question3 = new Question();
+        question3.questionStatement = "Qual a velocidade da luz no vácuo?";
+        question3.subject = "Física";
+        question3.setResolution("299.792.458 metros por segundo");
+
+        Test nerdExam = new Test(2, "Cultura pop");
+        nerdExam.teacher = "Douglas Adams";
+        nerdExam.addQuestion(question3);
+        nerdExam.addQuestion(question1);
+        nerdExam.addQuestion(question2);
+
+        Question[] nerdQuestions = nerdExam.getQuestions();
+        System.out.println("Questão 1");
+        System.out.println(nerdQuestions[0].questionStatement);
+        System.out.println("Questão 2");
+        System.out.println(nerdQuestions[1].questionStatement);
+
+        nerdExam.removeQuestion(1);
+        System.out.println("Questão 2 removida");
+        System.out.println(nerdQuestions[1].questionStatement);
     }
 }
